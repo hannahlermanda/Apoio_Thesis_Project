c755b2cc2ef1e79236ab97565e831ffc92bc5f26
hannahlermanda <hannahlermanda@gmail.com>
2024-12-02 17:27:09 -0800

    Set Up Navigate Card and Ride Options

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,9 +3,13 @@ import React from 'react';
 import tw from 'twrnc';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 const MapScreen = () => {
+    const Stack = createNativeStackNavigator();
+
   return (
     <View>
 
@@ -13,7 +17,25 @@ const MapScreen = () => {
             <Map />
         </View>  
 
-        <View style={tw `h-1/2`}></View>
+        <View style={tw `h-1/2`}>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="NavigateCard"
+                    component={NavigateCard}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="RideOptionsCard"
+                    component={RideOptionsCard
+                    }
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+            </Stack.Navigator>
+        </View>
 
     </View>
   )
